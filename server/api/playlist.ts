e9142e9874d95bc53bf7fe72aedfb73e85a4909d
Tim Benniks <tbenniks@gmail.com>
2024-06-16 10:54:18 +0200

    yolo

--- server/api/playlist.ts
+++ server/api/playlist.ts
@@ -6,21 +6,22 @@ function toVideo(video: any, fromPlaylist: boolean) {
     image: video.snippet.thumbnails?.high?.url.replace("hqdefault", "maxresdefault"),
     videoId: fromPlaylist ? video.snippet.resourceId.videoId : video.id.videoId ? video.id.videoId : video.id
   }
+}
 
-  export default defineEventHandler(async (event) => {
-    const { playlist_id, per_page } = getQuery(event)
+export default defineEventHandler(async (event) => {
+  const { playlist_id, per_page } = getQuery(event)
 
-    const data = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlist_id}&key=${process.env.YOUTUBE_KEY}&maxResults=${per_page || 5}`)
+  const data = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlist_id}&key=${process.env.YOUTUBE_KEY}&maxResults=${per_page || 5}`)
 
-    const result = await data.json()
-    let response;
+  const result = await data.json()
+  let response;
 
-    if (result.error) {
-      response = result.error
-    }
-    else {
-      response = result.items.map(video => (toVideo(video, true))).filter((video) => video.title !== 'Deleted video')
-    }
+  if (result.error) {
+    response = result.error
+  }
+  else {
+    response = result.items.map(video => (toVideo(video, true))).filter((video) => video.title !== 'Deleted video')
+  }
 
-    return { videos: response }
-  })
+  return { videos: response }
+})
