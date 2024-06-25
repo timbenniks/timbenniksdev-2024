8b7759983e6a9585cdcee5228b6466d78c93edd2
Tim Benniks <tbenniks@gmail.com>
2024-06-25 21:36:07 +0200

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -21,103 +21,90 @@ export default defineAppConfig({
     ]
   },
   timeline: {
-    items: [
-      {
-        "daterange": "August 2023 - Present",
-        "text": "",
-        "company": "Hygraph",
-        "title": "Developer Relations Lead for Outreach and Awareness",
-        "location": "Remote",
-        "url": "https://hygraph.com"
-      },
-      {
-        "daterange": "March 2021 - Present",
-        "text": "",
-        "company": "MACH Alliance",
-        "title": "Member of Tech Council",
-        "location": "Remote",
-        "url": "https://machalliance.org"
-      },
-      {
-        "daterange": "January 2023 - August 2023",
-        "text": "",
-        "company": "Uniform",
-        "title": "Director of Developer Relations",
-        "location": "Remote",
-        "url": "https://uniform.dev"
-      },
-      {
-        "daterange": "February 2021 - January 2023",
-        "text": "Joined Uniform as Principal Developer Advocate. Developer Relations, defining the perception of Uniform to technical people in the community of developers, partners, and customers. Setting up processes, KPIs, and their measurement, creating content (written and video) around DXC, and speaking at conferences.",
-        "company": "Uniform",
-        "title": "Principal Developer Advocate",
-        "location": "Remote",
-        "url": "https://uniform.dev"
-      },
-      {
-        "daterange": "March 2018 - February 2021",
-        "text": "Served as Director of Web Development at Valtech. A global role representing front-end developers across Valtech. Setting up best practices around team management, pitching, and use of MACH technology. Helping Global clients to achieve digital transformation at scale. Clients include: P&G, Rolex, LVMH",
-        "company": "Valtech",
-        "title": "Director of Web Development",
-        "location": "Paris Area, France",
-        "url": "https://www.valtech.com"
-      },
-      {
-        "daterange": "April 2017 - January 2020",
-        "text": "Global front-end lead on the website factory program for L'Oréal. Leading teams in France, US and Ukraine. In charge of code quality across projects, front-end architecture, tech stack decisions. Brands: Kerastase, Garnier, Redken, Essie, L'Oréal Paris, La Roche Posay, Vichy.",
-        "company": "Valtech",
-        "title": "Global Front-End Lead",
-        "location": "Paris Area, France",
-        "url": "https://www.valtech.com"
-      },
-      {
-        "daterange": "April 2015 - April 2017",
-        "text": "Served as Technical Delivery Manager at AKQA. Dividing time between being tech lead and developer. Streamlining client processes and decision making with creative innovation, tech limitations and client goals in mind. Clients: Chanel, Hermès, Google, Nike.",
-        "company": "AKQA",
-        "title": "Technical Delivery Manager",
-        "location": "Paris Area, France",
-        "url": "https://www.akqa.com"
-      },
-      {
-        "daterange": "March 2014 - April 2015",
-        "text": "Served as Principal Web Developer at AKQA. Leading the tech team on a content level, presenting to peers, guiding juniors and interns and working closely with external tech partners. Clients: Chanel, Hermès, Red Bull, Martell, Google, Nike",
-        "company": "AKQA",
-        "title": "Principal Web Developer",
-        "location": "Paris Area, France",
-        "url": "https://www.akqa.com"
-      },
-      {
-        "daterange": "January 2011 - March 2014",
-        "text": "Served as Senior Web Developer at AKQA. Worked on front-end architecture for big multi-market platforms and through the line campaigns. Clients: Heineken, Tommy Hilfiger, Mini, Nike, EA Sports, Philips.",
-        "company": "AKQA",
-        "title": "Senior Web Developer",
-        "location": "Amsterdam Area, Netherlands",
-        "url": "https://www.akqa.com"
-      },
-      {
-        "daterange": "May 2010 - December 2010",
-        "text": "Served as Front-End Developer at Mirabeau. Worked closely with the backend developers. Clients: Transavia.com, AutoTrader, VUmc, Nutricia, Stadgenoot, KLM, RDC.",
-        "company": "Mirabeau",
-        "title": "Front-End Developer",
-        "location": "Netherlands",
-        "url": "https://www.mirabeau.nl"
-      },
-      {
-        "daterange": "January 2010 - May 2010",
-        "text": "Served as Team Lead Front-End Development at Mediamatic. Responsible for the quality assurance of code, meeting deadlines, and handling resourcing.",
-        "company": "Mediamatic",
-        "title": "Team Lead Front-End Development",
-        "location": "Amsterdam",
-        "url": "https://www.mediamatic.net"
-      },
-      {
-        "daterange": "January 2008 - January 2010",
-        "text": "Served as Front-End Engineer at Mediamatic. Responsible for the overall look and feel of websites built with the anyMeta CMS. Clients: Picnic, Pakhuis de Zwijger, Women INC, Mediamatic.net, Mediamatic Travel, Museum de Paviljoens.",
-        "company": "Mediamatic",
-        "title": "Front-End Engineer",
-        "location": "Amsterdam",
-        "url": "https://www.mediamatic.net"
-      }
-    ]
+    items: [{
+      daterange: "August 2023 - Present",
+      text: "At Hygraph, Tim Benniks is the Developer Relations Lead, driving outreach and awareness initiatives. In this role, he connects with the developer community through engaging content, live streams, and industry events. Tim's efforts are focused on showcasing Hygraph's capabilities, fostering strong relationships with developers and partners, and promoting the adoption of headless CMS solutions. His work enhances Hygraph's visibility and empowers developers to build innovative and scalable applications using Hygraph's platform.",
+      company: "Hygraph",
+      title: "Developer Relations Lead for Outreach and Awareness",
+      location: "Remote",
+      url: "https://hygraph.com"
+    }, {
+      daterange: "March 2021 - Present",
+      text: "At the beginning of my tenure as a member of the Technical Council, I played a pivotal role in shaping the decision-making processes for new member admissions for SI's. Diligently evaluated potential members to ensure they adhered to Microservices, API-first, Cloud-native, and Headless (MACH) architecture principles. As a group of dedicated Council members, we were instrumental in maintaining the high standards of the alliance, fostering innovation, and ensuring that new entrants contributed meaningfully to the MACH ecosystem.",
+      company: "MACH Alliance",
+      title: "Member of Tech Council",
+      location: "Remote",
+      url: "https://machalliance.org"
+    }, {
+      daterange: "January 2023 - August 2023",
+      text: "Played a crucial role in engaging and educating the developer community. Inspire and Empower is the motto. Responsibilities included creating educational content, live streaming, speaking at conferences, and providing guidance to developers to effectively utilize Uniform's cutting-edge platform. My work significantly contributed to increasing Uniform's visibility and adoption within the tech community, fostering a vibrant and informed user base.",
+      company: "Uniform",
+      title: "Director of Developer Relations",
+      location: "Remote",
+      url: "https://uniform.dev"
+    }, {
+      daterange: "February 2021 - January 2023",
+      text: "Joined Uniform as Principal Developer Advocate. Developer Relations, defining the perception of Uniform to technical people in the community of developers, partners, and customers. Setting up processes, KPIs, and their measurement, creating content (written and video) around DXC, and speaking at conferences.",
+      company: "Uniform",
+      title: "Principal Developer Advocate",
+      location: "Remote",
+      url: "https://uniform.dev"
+    }, {
+      daterange: "March 2018 - February 2021",
+      text: "Served as Director of Web Development at Valtech. A global role representing front-end developers across Valtech. Setting up best practices around team management, pitching, and use of MACH technology. Helping Global clients to achieve digital transformation at scale. Clients include: P&G, Rolex, LVMH",
+      company: "Valtech",
+      title: "Director of Web Development",
+      location: "Paris Area, France",
+      url: "https://www.valtech.com"
+    }, {
+      daterange: "April 2017 - January 2020",
+      text: "Global front-end lead on the website factory program for L'Oréal. Leading teams in France, US and Ukraine. In charge of code quality across projects, front-end architecture, tech stack decisions. Brands: Kerastase, Garnier, Redken, Essie, L'Oréal Paris, La Roche Posay, Vichy.",
+      company: "Valtech",
+      title: "Global Front-End Lead",
+      location: "Paris Area, France",
+      url: "https://www.valtech.com"
+    }, {
+      daterange: "April 2015 - April 2017",
+      text: "Served as Technical Delivery Manager at AKQA. Dividing time between being tech lead and developer. Streamlining client processes and decision making with creative innovation, tech limitations and client goals in mind. Clients: Chanel, Hermès, Google, Nike.",
+      company: "AKQA",
+      title: "Technical Delivery Manager",
+      location: "Paris Area, France",
+      url: "https://www.akqa.com"
+    }, {
+      daterange: "March 2014 - April 2015",
+      text: "Served as Principal Web Developer at AKQA. Leading the tech team on a content level, presenting to peers, guiding juniors and interns and working closely with external tech partners. Clients: Chanel, Hermès, Red Bull, Martell, Google, Nike",
+      company: "AKQA",
+      title: "Principal Web Developer",
+      location: "Paris Area, France",
+      url: "https://www.akqa.com"
+    }, {
+      daterange: "January 2011 - March 2014",
+      text: "Served as Senior Web Developer at AKQA. Worked on front-end architecture for big multi-market platforms and through the line campaigns. Clients: Heineken, Tommy Hilfiger, Mini, Nike, EA Sports, Philips.",
+      company: "AKQA",
+      title: "Senior Web Developer",
+      location: "Amsterdam Area, Netherlands",
+      url: "https://www.akqa.com"
+    }, {
+      daterange: "May 2010 - December 2010",
+      text: "Served as Front-End Developer at Mirabeau. Worked closely with the backend developers. Clients: Transavia.com, AutoTrader, VUmc, Nutricia, Stadgenoot, KLM, RDC.",
+      company: "Mirabeau",
+      title: "Front-End Developer",
+      location: "Netherlands",
+      url: "https://www.mirabeau.nl"
+    }, {
+      daterange: "January 2010 - May 2010",
+      text: "Served as Team Lead Front-End Development at Mediamatic. Responsible for the quality assurance of code, meeting deadlines, and handling resourcing.",
+      company: "Mediamatic",
+      title: "Team Lead Front-End Development",
+      location: "Amsterdam",
+      url: "https://www.mediamatic.net"
+    }, {
+      daterange: "January 2008 - January 2010",
+      text: "Served as Front-End Engineer at Mediamatic. Responsible for the overall look and feel of websites built with the anyMeta CMS. Clients: Picnic, Pakhuis de Zwijger, Women INC, Mediamatic.net, Mediamatic Travel, Museum de Paviljoens.",
+      company: "Mediamatic",
+      title: "Front-End Engineer",
+      location: "Amsterdam",
+      url: "https://www.mediamatic.net"
+    }]
   }
 })
