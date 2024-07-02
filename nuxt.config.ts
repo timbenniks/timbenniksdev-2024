44f7d9ffb50e52618ffa78b6fe279e91e58bb78d
Tim Benniks <tbenniks@gmail.com>
2024-07-02 11:27:57 +0200

    added feed and redirects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
 
   nitro: {
     prerender: {
-      routes: ['/sitemap.xml']
+      routes: ['/sitemap.xml', '/feed.xml']
     }
   },
 
---
+++ server/routes/feed.xml.ts
@@ -0,0 +1,26 @@
+import RSS from 'rss';
+import { serverQueryContent } from '#content/server'
+
+export default defineEventHandler(async (event) => {
+
+  const feed = new RSS({
+    title: 'Tim Benniks',
+    site_url: 'https://timbenniks.dev',
+    feed_url: `https://timbenniks.dev/feed.xml`,
+  });
+
+  const docs = await serverQueryContent(event, "/writing").find()
+
+  docs.map((post: any) => {
+    feed.item({
+      title: post.title,
+      url: `https://timbenniks.dev${post._path}`,
+      description: post.description,
+      date: post.date,
+      categories: post.tags,
+      author: "Tim Benniks",
+    });
+  })
+
+  return feed.xml({ indent: true });
+})
