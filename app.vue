054edffdb180e37c6b78d225ca05a8e77dd29e1e
Tim Benniks <tbenniks@gmail.com>
2025-11-08 07:41:42 +0100

    feat: integrate jstag for enhanced analytics tracking and page view monitoring

--- app.vue
+++ app.vue
@@ -49,16 +49,15 @@ useHead({
   },
 
   script: [
-    // {
-    //   type: "speculationrules",
-    //   innerHTML: `
-    //     {
-    //       "prerender": [{
-    //         "urls": ["/", "/about", "/videos", "/writing", "/speaking", "/livestreams", "/presskit", "/alive-and-kicking", "/uses"]
-    //       }]
-    //     }
-    //   `,
-    // },
+    {
+      innerHTML: `
+      !function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
+      jstag.init({
+        src: 'https://c.lytics.io/api/tag/c4e22848499cfc31ae98dd92f276115a/latest.min.js',
+      });
+    `,
+    },
+
     {
       defer: true,
       src: "/_vercel/speed-insights/script.js",
@@ -128,6 +127,14 @@ useJsonld({
     },
   ],
 });
+
+watch(
+  () => route.path,
+  (newPath, oldPath) => {
+    //@ts-ignore
+    window.jstag.pageView();
+  }
+);
 </script>
 <template>
   <main class="mx-auto max-w-[1400px] bg-[#1b1d39]">
